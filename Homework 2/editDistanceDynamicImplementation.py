617d403c3b14f757e53526c87b2e10f67144b13b
Owen Bezick <owbezick@davidson.edu>
2020-03-23 12:18:45 -0400

    comments

--- Homework 2/editDistanceDynamicImplementation.py
+++ Homework 2/editDistanceDynamicImplementation.py
@@ -6,54 +6,68 @@
 
     @authors: Owen Bezick & Morgan Dunnigan
 
+
+- Overall, the algorithm is correct in structure
+- I couldn't run the tests GATTACA->BUATTAA and GATTACA->ATACAB 
+- If you try strings of different length, you have a problem on line 27. 
+You invert the indexes of source/dest in the for loop 
+- In your reconstruction of the alignment, you should have three indexes,
+ one for the source, another for the destination, and one for the operation 
+ that you are currently examining.
+ 
 """
+
+
 def dynamicEditDistance(source, destination):
-    # create tables
-    notSecrettable = [[0 for x in range(1+ len(destination))] for y in range(1+len(source))]
-    secretTable = [[0 for x in range(1+ len(destination))] for y in range(1+len(source))]
-    notSecrettable[0][0] = 0
-
-    # initialize 1st row and column with copy or deletion cost
+    # Instantiate Tables
+    costTable = [[0 for x in range(1+ len(destination))] for y in range(1+len(source))]
+    costTable[0][0] = 0
+    transformationTable = [[0 for x in range(1+ len(destination))] for y in range(1+len(source))]
+    
+    # Fill in first row and column
     for i in range(1, 1 + len(destination)):
-        notSecrettable[0][i] = 1 + notSecrettable[0][i-1]
-        secretTable[0][i] = 'D'
-
+        costTable[0][i] = 1 + costTable[0][i-1]
+        transformationTable[0][i] = 'D'
     for i in range(1, 1 + len(source)):
-        notSecrettable[i][0] = notSecrettable[i-1][0] + 1
-        secretTable[i][0] = 'I'
-
+        costTable[i][0] = costTable[i-1][0] + 1
+        transformationTable[i][0] = 'I'
+    
+    # Iterate over source and destination and fill in tables
     for i in range(1, 1 + len(destination)):
         for j in range(1,1 +  len(source)):
+            # If transformation is a copy
             if destination[j-1] == source[i-1]:
-                notSecrettable[j][i] = notSecrettable[j-1][i-1]
-                secretTable[j][i] = 'C'
-
+                costTable[j][i] = costTable[j-1][i-1]
+                transformationTable[j][i] = 'C'
             else:
-                deleteCost = notSecrettable[j][i-1] + 1
-                insertCost = notSecrettable[j-1][i] + 1
-                replaceCost = notSecrettable[j-1][i-1] + 1.5
-                minCost = min(deleteCost, insertCost, replaceCost)
-
-                notSecrettable[j][i] = minCost
-
+                # Create Costs
+                deleteCost = costTable[j][i-1] + 1
+                insertCost = costTable[j-1][i] + 1
+                replaceCost = costTable[j-1][i-1] + 1.5
+                # Find minimum cost
+                minCost = min(deleteCost, insertCost, replaceCost) 
+                # Place in cost table
+                costTable[j][i] = minCost
+                # Fill transformation table
                 if minCost == replaceCost:
-                    secretTable[j][i] = 'R'
+                    transformationTable[j][i] = 'R'
                 elif minCost == deleteCost:
-                    secretTable[j][i] = 'D'
+                    transformationTable[j][i] = 'D'
                 else:
-                    secretTable[j][i] = 'I'
+                    transformationTable[j][i] = 'I'
 
-
+    
+    # Create the string of transformations
     i = len(source)
     j = len(destination)
-    transformationString =  secretTable[i][j]
+    transformationString =  transformationTable[i][j]
     while i != 0 and j != 0:
-        left = notSecrettable[i][j - 1]
-        top = notSecrettable[i - 1][j]
-        diag = notSecrettable[i -1][j -1]
+        left = costTable[i][j - 1]
+        top = costTable[i - 1][j]
+        diag = costTable[i -1][j -1]
         minCost = min(left, top, diag)
 
-        if diag == notSecrettable[i][j]:
+        if diag == costTable[i][j]:
             transformationString = 'C' + transformationString
             i = i - 1
             j = j - 1
@@ -68,12 +82,11 @@ def dynamicEditDistance(source, destination):
             i = i - 1
             j = j - 1
 
-
+    # Trace back through transition table to get the new string
     newString = ''
     i = 0
     c = 0
     while i < len(transformationString):
-        print(newString)
         if transformationString[i] == 'C':
             newString = newString + destination[c]
             i+=1
@@ -90,9 +103,10 @@ def dynamicEditDistance(source, destination):
             newString = newString + ' '
             i+=1
 
+    # Print out results
     print('Transformations:', transformationString)
-    print('New String', newString)
-    print('Cost:', notSecrettable[len(source)][len(destination)])
+    print('New String:', newString)
+    print('Cost:', costTable[len(source)][len(destination)])
 
 
 # Wrapper function
@@ -100,6 +114,7 @@ def main():
     source = "GATTACA"
     destination = "CATACAC"
     dynamicEditDistance(source, destination)
+
 
 if __name__ == '__main__':
     main()
